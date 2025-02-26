5322f44163a0d9d849efcf02b29a509efbe01cb6
Quincy van Deursen <q.vandeursen@student.avans.nl>
2025-02-26 19:38:17 +0100

    Landing page

--- VoedIQ/src/app/app.routes.ts
+++ VoedIQ/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LandingComponent } from './features/landing/landing.component';
 import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/landing', pathMatch: 'full' }, // Standaard naar landing
-    { path: 'landing', component: LandingComponent },
-    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/landing', pathMatch: 'full' },  // Standaard naar landing
+    { path: 'landing', component: LandingComponent},
+    { path: 'home', component: HomeComponent},  // Hier expliciet 'true' toevoegen
 ];
--- VoedIQ/src/app/features/landing/landing.component.ts
+++ VoedIQ/src/app/features/landing/landing.component.ts
@@ -40,5 +40,19 @@ export class LandingComponent implements OnInit {
     }
   }
 
+  onKeydown(event: KeyboardEvent): void {
+    const key = event.key;
+
+    // Laat alleen letters (A-Z, a-z), spaties, en Backspace door
+    if (!/^[a-zA-Z\s]$/.test(key) && key !== 'Backspace') {
+      event.preventDefault();  // Voorkom dat andere toetsen worden ingevoerd
+    }
+
+    // Als de Enter toets wordt ingedrukt en het invoerveld is niet leeg
+    if (key === 'Enter' && this.name) {
+      this.submitName();  // Trigger de submitfunctie als Enter wordt ingedrukt
+    }
+  }
+
 
 }
