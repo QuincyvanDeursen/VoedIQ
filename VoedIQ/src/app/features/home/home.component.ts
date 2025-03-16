fe2e2cb704a07b39fecec7c40ba8d72acab95f06
Quincy van Deursen <q.vandeursen@student.avans.nl>
2025-03-16 22:44:05 +0100

    Homepage

--- VoedIQ/src/app/features/home/home.component.ts
+++ VoedIQ/src/app/features/home/home.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { card } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, card],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   userName: string = '';
   greeting: string = this.setGreeting();
-  
+
   ngOnInit() {
     this.userName = localStorage.getItem('userName') || '';
   }
@@ -19,5 +21,14 @@ export class HomeComponent {
     return ['Goedenacht', 'Goedemorgen', 'Goedemiddag', 'Goedenavond'][
       hour < 5 ? 0 : hour < 12 ? 1 : hour < 18 ? 2 : 3
     ];
+  }
+
+  scrollToElement(section: string) {
+    const element = document.getElementById(section);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth', // Zorgt voor een soepele scroll
+      });
+    }
   }
 }
